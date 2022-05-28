0a34bea6c977a718b83989cc5ba128ee8837af85
mayerfonseca <fonsecamayer2@gmail.com>
2022-05-27 19:56:39 -0600

    subiendo cambios

---
+++ js/geolocalizacion.js
@@ -0,0 +1,37 @@
+function geolocalizacion() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(geo_success,error);
+    } else {
+        alert("Geolocalización no soportada en su navegador");
+    }
+}
+
+function geo_success(position) {
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
+    var accuracy = position.coords.accuracy;
+
+    var mymap = document.getElementById("map");
+    mymap.style.height = "30rem";
+    mapa(lon, lat, accuracy);
+}
+
+function error(){
+    alert("No se pudo obtener la ubicación");
+}
+
+function mapa(longitude, latitude, accuracy) {
+
+    var map = L.map('map').setView([latitude, longitude], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright%22%3EOpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([latitude, longitude]).addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+
+}
+
+window.onload = geolocalizacion();
