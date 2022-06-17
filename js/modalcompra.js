b6a348279b25faf8de869b14d11c92f161b42c57
mayerfonseca <fonsecamayer2@gmail.com>
2022-06-17 07:53:03 -0600

    validaciones

--- js/modalcompra.js
+++ js/modalcompra.js
@@ -7,7 +7,26 @@ const openfail = document.getElementById('openfail');
 const modal_container_fail = document.getElementById('modal_container_fail');
 const closefail = document.getElementById('closefail');
 
+function validarCamposVacios(event) {
+    var elements = document.getElementById("myform").elements;
+    var flag = true;
+
+    for (var i = 0, element; element = elements[i++];) {
+        if ((element.type === "text" && element.value === "") || (element.type === "tel" && element.value === "") || (element.type === "number" && element.value === "") || (element.type === "textarea" && element.value === "") || (element.type === "email" && element.value === "") || (element.type === "date" && element.value === "")) {
+            flag = false;
+            event.preventDefault();
+            document.getElementsByClassName("MensajeValidacion")[0].style.display = "block";
+            break;
+        }
+    }
+
+    return flag;
+}
+
 open.addEventListener('click', () => {
+    if(validarCamposVacios(event)===false){
+        return;
+    }
     modal_container.classList.add('show');
     disableScrolling();
 });
